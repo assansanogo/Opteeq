dd7a985bbaff20b3efc73d5dc63533dc28d66c8b
souff <leo.bonnand.germain@wanadoo.fr>
2021-08-12 21:12:08 +0200

    auto set image path folder in via.json after download

--- download.py
+++ download.py
@@ -9,7 +9,7 @@ import json
 def main(annotator_name: str, bucket_json: str, bucket_image,
          local_storage: str = "download") -> None:
     """
-    download via json and image which aren't store in local.
+    download via json and image which aren't store in local. Edit json with path image folder.
 
     :param annotator_name: name of the user
     :param bucket_json: bucket where json via are stored
@@ -19,12 +19,18 @@ def main(annotator_name: str, bucket_json: str, bucket_image,
     bucket = Bucket(bucket_image)
     json_set = download_via_json(annotator_name, bucket_json, local_storage)
     for json_name in json_set:
-        with open(os.path.join(local_storage, "json", json_name), "r") as f:
-            json_via = json.load(f)
+        json_path = os.path.join(local_storage, "json", json_name)
+        with open(json_path, "r") as file:
+            json_via = json.load(file)
             path_folder = os.path.join(local_storage, "image", json_name.split(".")[0])
             os.mkdir(path_folder)
             for key in json_via["_via_img_metadata"].keys():
                 bucket.download(key, path_folder)
+        # auto set image path folder
+        with open(json_path, "w") as file:
+            json_via["_via_settings"]["core"]["default_filepath"] = os.path.join(
+                os.path.abspath(path_folder), "")
+            json.dump(json_via, file)
 
 
 def download_via_json(annotator_name: str, bucket_name: str,
