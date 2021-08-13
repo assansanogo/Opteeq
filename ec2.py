ffaea9c9f1ff0de998b6ce587a493827a9bd9dc6
souff <leo.bonnand.germain@wanadoo.fr>
2021-08-13 20:50:55 +0200

    add project name in each via json (for export)

--- ec2.py
+++ ec2.py
@@ -29,7 +29,9 @@ def main(region: str, table_name: str, batch_size: int, bucket_in: str, bucket_o
         for i in tqdm(range(len(key_list) // batch_size), desc="batch", leave=False):
             batch = key_list[i * batch_size: (i + 1) * batch_size]
             json = via_json(batch, bucket_in, profile=profile)
-            annotator = bucket.put_object_annotator(json, f'{int(time.time())}_{i}.json')
+            file_name = f'{int(time.time())}_{i}.json'
+            json['_via_settings']["project"]["name"] = file_name.split(".")[0]
+            annotator = bucket.put_object_annotator(json, file_name)
             # todo find if it is possible to update all in one
             for key in batch:
                 table.update_annotator(key, annotator)
