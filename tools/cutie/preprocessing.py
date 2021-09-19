f84a995eb19473f498dbfabad452114aaba8bb42
johann yates <johannyates@gmail.com>
2021-09-19 21:57:19 +0200

    recentered the grid on the text area

--- tools/cutie/preprocessing.py
+++ tools/cutie/preprocessing.py
@@ -7,11 +7,11 @@ import os
 
 def divide_boxes(via_file: str) -> dict:
     """Divides the boxes containing several words in the via annotations file given as parameter.
-    Returns a dictionnary with the information of each box : filename, class, word, x, y, width, height.
+    Returns a dictionary with the information of each box : filename, class, word, x, y, width, height.
 
     :param via_file: path to the via csv file
     :type via_file: str
-    :return: dictionnary with the boxes information
+    :return: dictionary with the boxes information
     :rtype: dict
     """
     via_clean = clean_via_file(via_file) # clean the initial file to get the right separators
@@ -61,12 +61,12 @@ def divide_boxes(via_file: str) -> dict:
 
     return(final_dict)
 
-def check_box_dictionnary(image_path: str, box_dict: dict):
-    """Opens a new window showing the image and the boxes found in the dictionnary given as parameter.
+def check_box_dictionary(image_path: str, box_dict: dict):
+    """Opens a new window showing the image and the boxes found in the dictionary given as parameter.
     
     :param image_path: path to the image
     :type image_path: str
-    :param box_dict: dictionnary prepared with divide_boxes function
+    :param box_dict: dictionary prepared with divide_boxes function
     :type box_dict: dict
     """
     filename = os.path.basename(image_path)
@@ -98,19 +98,19 @@ def check_box_dictionnary(image_path: str, box_dict: dict):
     cv2.destroyAllWindows()
 
 def generate_grids(image_dir: str, box_dict: dict, nb_cols: int, nb_lines : int) -> dict:
-    """Generates a grid of words per image, based on the boxes dictionnary given as parameter.
-    Returns the grid information in a dictionnary with the format :
+    """Generates a grid of words per image, based on the boxes dictionary given as parameter.
+    Returns the grid information in a dictionary with the format :
     { image_name : { (col,line) : [word, class] } }
     
     :param image_dir: images directory path
     :type image_dir: str
-    :param box_dict: dictionnary prepared with divide_boxes function
+    :param box_dict: dictionary prepared with divide_boxes function
     :type box_dict: dict
     :param nb_cols: number of columns wanted in the grid
     :type nb_cols: int
     :param nb_lines: number of lines wanted in the grid
     :type nb_lines: int
-    :return: dictionnary containing the coordinates of the words in the grid 
+    :return: dictionary containing the coordinates of the words in the grid 
     :rtype: dict
 
     """
@@ -118,18 +118,31 @@ def generate_grids(image_dir: str, box_dict: dict, nb_cols: int, nb_lines : int)
     grid = {}
     for image in images:
         grid[image] = {}
-        im_height, im_width, _ = get_img_shape(os.path.join(image_dir,image))
+        
         indexes = [idx for (idx, name) in enumerate(box_dict['filename']) if name == image]
+        x_values = [x for (idx, x) in enumerate(box_dict['x']) if idx in indexes]
+        y_values = [x for (idx, x) in enumerate(box_dict['y']) if idx in indexes]
+
+        xmin = min(x_values) - 1
+        xmax = max(x_values) + 1
+        ymin = min(y_values) - 1
+        ymax = max(y_values) + 1
+
+        im_height, im_width, _ = get_img_shape(os.path.join(image_dir,image))
+        ticket_height = ymax - ymin
+        ticket_width = xmax - xmin
+
         for idx in indexes:
-            word_col = int((box_dict['x'][idx] / im_width) * nb_cols)
-            word_line = int((box_dict['y'][idx] / im_height) * nb_lines)
+            word_col = int(((box_dict['x'][idx] - xmin) / ticket_width) * nb_cols)
+            word_line = int(((box_dict['y'][idx] - ymin) / ticket_height) * nb_lines)
             grid[image][(word_col,word_line)] = [box_dict['word'][idx], box_dict['class'][idx]]
     return grid
 
 def visualize_grid(gridmap: dict, nb_cols: int, nb_lines : int, filename: str):
-    """TBD
+    """Opens a new window showing the visual representation of the image given as parameter.
+    Image grid has to be prepared with the generate_grids function and given as parameter.
     
-    :param gridmap: dictionnary prepared with generate_grids function
+    :param gridmap: dictionary prepared with generate_grids function
     :type gridmap: dict
     :param nb_cols: number of columns in the grids
     :type nb_cols: int
@@ -139,34 +152,35 @@ def visualize_grid(gridmap: dict, nb_cols: int, nb_lines : int, filename: str):
     :type filename: str
 
     """
-    image = np.zeros((800,800,3), np.uint8) + 255
+    image = np.zeros((620,620,3), np.uint8) + 255
     font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-    fontScale = 0.3
+    fontScale = 0.5
     thickness = 1
     for key, value in gridmap[filename].items():
-        x = int((key[0] * 800) / nb_cols)
-        y = int((key[1] * 800) / nb_lines)
+        x = int((key[0] * 600) / nb_cols)
+        y = int((key[1] * 600) / nb_lines) + 10
         cell_word = value[0]
         cell_class = value[1]
         color = ((cell_class==1)*50 + (cell_class==2)*255, (cell_class==1)*150 + (cell_class==3)*255\
                         ,(cell_class==1)*255 + (cell_class==4)*255)
         cv2.putText(image,cell_word,(x, y),font,fontScale,color,thickness)
-    for line_idx in range(nb_lines + 1):
-        start_point = (0, int(line_idx*800/nb_lines)+5)
-        end_point = (1000, int(line_idx*800/nb_lines)+5)
+    for line_idx in range(nb_lines):
+        start_point = (0, int(line_idx*600/nb_lines) + 10)
+        end_point = (620, int(line_idx*600/nb_lines) + 10)
         cv2.line(image, start_point, end_point, (0,0,0), 1)
-    for col_idx in range(nb_cols + 1):
-        start_point = (int(col_idx*800/nb_cols - 5), 0)
-        end_point = (int(col_idx*800/nb_cols) - 5, 1000)
+    for col_idx in range(nb_cols):
+        start_point = (int(col_idx*600/nb_cols + 30), 0)
+        end_point = (int(col_idx*600/nb_cols) + 30, 620)
         cv2.line(image, start_point, end_point, (0,0,0), 1)
 
     cv2.imshow(filename, image)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-#box_dict = divide_boxes('tools/cutie/1629378395_1_csv.csv')
-#check_box_dictionnary('tools/yolo/data/f94dd838-259e-4ac6-811c-92c61b0d80c4.jpg', box_dict)
-#mygrid = generate_grids('tools/yolo/data',box_dict, 20, 50)
-#filename = '5d87e1bb-31fa-4e5e-baec-34dfbf937a99.jpg'
-#visualize_grid(mygrid, 20, 50, filename)
+# box_dict = divide_boxes('tools/cutie/1629378395_1_csv.csv')
+# check_box_dictionary('tools/yolo/data/f94dd838-259e-4ac6-811c-92c61b0d80c4.jpg', box_dict)
+# mygrid = generate_grids('tools/yolo/data',box_dict, 15, 30)
+# files = [file for file in os.listdir('tools/yolo/data') if file[-1] == 'g']
+# filename = '5d87e1bb-31fa-4e5e-baec-34dfbf937a99.jpg'
+# visualize_grid(mygrid, 15, 30, files[1])
 
