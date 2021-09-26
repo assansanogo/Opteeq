f11b1e212e42a938e875e06f42116cc8b574ddeb
johann yates <johannyates@gmail.com>
2021-09-26 21:42:27 +0200

    Blurring and noising data functions added

--- Image_rotation_cnn/prepare_functions.py
+++ Image_rotation_cnn/prepare_functions.py
@@ -1,36 +1,55 @@
-from tools.image.imageTools import resize_and_pad, rotate_image, image_read
+from tools.image.imageTools import resize_and_pad, rotate_image, image_read, add_blurring, add_noise
+import random
 import cv2
 import os
 
 
 def prepare_1_image(file: str, filename: str, out_folder: str):
-    """Resizes an image and rotate it 3 times to get 4 images that can be used to train the model.
+    """Resizes an image, rotates it 3 times, adds blurring and noise to get 12 images that
+    can be used to train a model.
 
     :param file: path to the original image
     :type file: str
-    :param filename: filename base of the 4 processed images
+    :param filename: filename base of the processed images
     :type filename: str
-    :param out_folder: path to the folder to upload the 4 processed images
+    :param out_folder: path to the folder to upload the 12 processed images
     :type out_folder: str
     """
     img = image_read(file)
-
+    # Initializing random parameters for blurring and noising functions
+    box_widths = random.choices([3,5,7,9],k=4)
+    box_heights = random.choices([3,5,7,9],k=4)
+    noise_vars = [random.randint(25,1000) for i in range(4)]
+    
     resized_img = resize_and_pad(img, desired_size=180)
-    rotated_img_90 = rotate_image(resized_img)
-    rotated_img_180 = rotate_image(rotated_img_90)
-    rotated_img_270 = rotate_image(rotated_img_180)
-    os.path.join(out_folder, f'{filename}_000.jpg')
-    cv2.imwrite(os.path.join(out_folder, f'{filename}_000.jpg'), resized_img)
-    cv2.imwrite(os.path.join(out_folder, f'{filename}_090.jpg'), rotated_img_90)
-    cv2.imwrite(os.path.join(out_folder, f'{filename}_180.jpg'), rotated_img_180)
-    cv2.imwrite(os.path.join(out_folder, f'{filename}_270.jpg'), rotated_img_270)
+    
+    for index, angle in enumerate(['000', '090', '180', '270']):
+        if index != 0:
+            resized_img = rotate_image(resized_img)
+        resized_img_b = add_blurring(resized_img,box_widths[index],box_heights[index])
+        resized_img_n = add_noise(resized_img, noise_vars[index])
+        cv2.imwrite(os.path.join(out_folder, f'{filename}_{angle}.jpg'), resized_img)
+        cv2.imwrite(os.path.join(out_folder, f'{filename}_b_{angle}.jpg'), resized_img_b)
+        cv2.imwrite(os.path.join(out_folder, f'{filename}_n_{angle}.jpg'), resized_img_n)
+
+    #resized_img = resize_and_pad(img, desired_size=180)
+    #resized_img_b = add_blurring(resized_img,box_widths[0])
+    #rotated_img_90 = rotate_image(resized_img)
+    #rotated_img_180 = rotate_image(rotated_img_90)
+    #rotated_img_270 = rotate_image(rotated_img_180)
+    #os.path.join(out_folder, f'{filename}_000.jpg')
+    #cv2.imwrite(os.path.join(out_folder, f'{filename}_000.jpg'), resized_img)
+    #cv2.imwrite(os.path.join(out_folder, f'{filename}_090.jpg'), rotated_img_90)
+    #cv2.imwrite(os.path.join(out_folder, f'{filename}_180.jpg'), rotated_img_180)
+    #cv2.imwrite(os.path.join(out_folder, f'{filename}_270.jpg'), rotated_img_270)
 
 def prepare_training_images(in_folder: str, out_folder: str) -> 'tuple[int, list[str]]':
-    """Uses all image files in a folder to get 4 images / file that can be used to train the model.
+    """Uses all image files in a folder to get 12 images / file that can be used to train a model.
     The N images in the original folder must all be in the normal orientation.
-    The 4xN processed images in the output folder are squared with size 180 and rotated in 4
-    orientations (0, 90, 180, and 270 degrees). The last 3 characters of the file names indicates the
-    orientation : '000', '090', '180' or '270'.
+    The 12xN processed images in the output folder are squared with size 180, rotated in 4
+    orientations (0, 90, 180, and 270 degrees) and with additional noise or blurring.
+    The last 3 characters of the file names indicate the orientation : '000',
+     '090', '180' or '270'.
 
     :param in_folder: path to the folder with original images
     :type in_folder: str
@@ -46,9 +65,14 @@ def prepare_training_images(in_folder: str, out_folder: str) -> 'tuple[int, list
             file_path = os.path.join(in_folder, filename)
             filename_base = filename.split('.')[0]
             prepare_1_image(file_path, filename_base, out_folder)
-            file_counter += 4
+            file_counter += 12
         except:
             not_worked.append(filename)
 
     return (file_counter, not_worked)
 
+
+
+prepare_1_image('Image_rotation_cnn/Original/20210913_122444.jpg', 'bob', 'Image_rotation_cnn/Original/')
+
+bob, listbob = prepare_training_images('Image_rotation_cnn/Original/', 'Image_rotation_cnn/Original/')
--- tools/image/imageTools.py
+++ tools/image/imageTools.py
@@ -57,6 +57,39 @@ def rotate_image(img: np.ndarray, angle: int = 90) -> np.ndarray:
     rotated = cv2.warpAffine(img, M, (w, h))
     return rotated
 
+def add_blurring(image: np.ndarray, box_width: int = 3, box_height: int = 3) -> np.ndarray:
+    """Adds blur to an image with an averaging convolutional filter.
+
+    :param img: Image array
+    :type img: np.ndarray
+    :param box_width: Width of the convolutional filter, default = 3
+    :type box_width: int, optional
+    :param box_height: Height of the convolutional filter, default = 3
+    :type box_height: int, optional
+    :return: Blurred image array
+    :rtype: np.ndarray
+    """
+    image_blurred = cv2.blur(image,(box_width, box_height))
+    return image_blurred
+
+def add_noise(image: np.ndarray, noise_variance: int = 25) -> np.ndarray:
+    """Adds a gaussian noise to an image with mean 0 and variance noise_variance.
+
+    :param img: Image array
+    :type img: np.ndarray
+    :param noise_variance: Variance of the gaussian noise to be added, default = 25
+    :type noise_variance: int, optional
+    :return: Noisy image array
+    :rtype: np.ndarray
+    """  
+    row,col,ch= image.shape
+    mean = 0
+    sigma = noise_variance**0.5
+    gauss = np.random.normal(mean,sigma,(row,col,ch))
+    gauss = gauss.reshape(row,col,ch)
+    noisy_image = image + gauss
+    return noisy_image
+    
 def get_img_shape(file: str) -> 'tuple[int, int, int]':
     """Returns the shape of an image in a tuple (height, width, #channels).
 
